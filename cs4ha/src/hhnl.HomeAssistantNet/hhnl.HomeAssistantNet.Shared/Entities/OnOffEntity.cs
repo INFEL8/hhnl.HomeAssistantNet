b131eb3827eb67240ad03c9d1623abdf1e336e1d
Тропаков Александр Владимирович <infel8@yandex.ru>
2025-11-21 19:46:10 +0300

    Domain as prop

--- cs4ha/src/hhnl.HomeAssistantNet/hhnl.HomeAssistantNet.Shared/Entities/OnOffEntity.cs
+++ cs4ha/src/hhnl.HomeAssistantNet/hhnl.HomeAssistantNet.Shared/Entities/OnOffEntity.cs
@@ -1,32 +1,33 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+
 using hhnl.HomeAssistantNet.Shared.HomeAssistantConnection;
 
 namespace hhnl.HomeAssistantNet.Shared.Entities
 {
     public abstract class OnOffEntity : BinaryEntity
     {
-        private readonly string _domain;
+        public string Domain { get; private set; }
 
         protected OnOffEntity(string uniqueId, IHomeAssistantClient assistantClient, string domain) : base(uniqueId,
             assistantClient)
         {
-            _domain = domain;
+            Domain = domain;
         }
 
         public async Task TurnOffAsync(CancellationToken cancellationToken = default)
         {
-            await HomeAssistantClient.CallServiceAsync(_domain, "turn_off", new { entity_id = UniqueId }, cancellationToken);
+            await HomeAssistantClient.CallServiceAsync(Domain, "turn_off", new { entity_id = UniqueId }, cancellationToken);
         }
 
         public async Task TurnOnAsync(CancellationToken cancellationToken = default)
         {
-            await HomeAssistantClient.CallServiceAsync(_domain, "turn_on", new { entity_id = UniqueId }, cancellationToken);
+            await HomeAssistantClient.CallServiceAsync(Domain, "turn_on", new { entity_id = UniqueId }, cancellationToken);
         }
 
         public async Task ToggleAsync(CancellationToken cancellationToken = default)
         {
-            await HomeAssistantClient.CallServiceAsync(_domain, "toggle", new { entity_id = UniqueId }, cancellationToken);
+            await HomeAssistantClient.CallServiceAsync(Domain, "toggle", new { entity_id = UniqueId }, cancellationToken);
         }
 
         public Task SetAsync(bool on, CancellationToken cancellationToken = default)
